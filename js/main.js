9a569482f389a2efbe0e12bb90b1671d0f19a909
abc <abc@example.com>
2019-09-10 23:18:52 -0700

    practice code

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+let cardOne = "Queen";
+console.log("User flipped " + cardOne);
+
+let cardTwo = "Queen";
+console.log("User flipped " + cardTwo);
+
+//let cardThree = "King";
+//let cardFour = "King";
+
+
