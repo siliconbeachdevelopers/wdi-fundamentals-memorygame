daef5ad1b8614f09998782fa8bd99d53d7a41aee
abc <abc@example.com>
2019-09-11 19:05:50 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 
 
-let cardOne = "Queen";
+/*let cardOne = "Queen";
 console.log("User flipped " + cardOne);
 
 let cardTwo = "Queen";
 console.log("User flipped " + cardTwo);
 
-//let cardThree = "King";
-//let cardFour = "King";
+let cardThree = "King";
+let cardFour = "King";*/
 
 
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [] 
+
+const cardOne = 'queen';{
+cards.push('cardOne');
+console.log("User flipped queen");}
+
+const cardTwo = 'king';{
+cards.push('cardTwo');
+console.log("User flipped king");}
+
+ 
+
+ if (cardsInPlay.length === 2);{
+    console.log("yes")}
+
+ if (cardsInPlay[0] === cardsInPlay[1]){
+	 alert("Sorry, try again");}
+ 
+  else {
+ 	alert("you've found a match!.");
+ }
+
+
+
+//cards[0];
+//cards;
